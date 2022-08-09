f05b5ccd1a8d45163429ec9a7ec6174e33c9520e
Grigor <gmmarmenia@gmail.com>
2022-08-09 17:48:39 +0400

    Adds test folder

---
+++ test/test.cpp
@@ -0,0 +1,38 @@
+/*
+void Test()
+{
+    OrderBook myOrderBook;
+
+    //auto & orders = myOrderBook.getBuyersOrders();
+
+    Order order_1("T1", 'B', 5 , 30);
+    Order order_2("T2", 'B', 5 , 70);
+    Order order_3("T3", 'B', 1 , 40);
+    Order order_4("T4", 'B', 2 , 60);
+    Order order_5("T5", 'B', 3 , 70);
+    Order order_6("T6", 'B', 20,  80);
+    Order order_7("T7", 'B', 1, 50);
+    Order order_8("T2", 'B', 5, 70);
+    Order order_9("T1", 'B', 1, 50);
+    Order order_10("T1", 'B', 3, 60);
+    Order order_11("T7", 'S', 2, 50);
+    Order order_12("T8", 'B', 10, 90);
+
+    myOrderBook.addOrder(order_1);
+    myOrderBook.addOrder(order_2);
+    myOrderBook.addOrder(order_3);
+    myOrderBook.addOrder(order_4);
+    myOrderBook.addOrder(order_5);
+    myOrderBook.addOrder(order_6);
+    myOrderBook.addOrder(order_7);
+    myOrderBook.addOrder(order_8);
+    myOrderBook.addOrder(order_9);
+    myOrderBook.addOrder(order_10);
+    myOrderBook.addOrder(order_11);
+    myOrderBook.addOrder(order_12);
+
+
+    output(myOrderBook.getBuyersOrders().getContainer());
+}
+
+*/
